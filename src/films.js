8c1ed6139932efbd4bc5130e7f4e30ae5100077c
Isabel Moreno <isabeltmorenot@gmail.com>
2022-06-29 23:14:03 +0200

    Nivel-1

--- src/films.js
+++ src/films.js
@@ -1,7 +1,6 @@
 
 // Exercise 1: Get the array of all directors.
 function getAllDirectors(array) {
-
   let result=array.map((element) => {
     return element.director;
   })
@@ -13,11 +12,7 @@ function getAllDirectors(array) {
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(array, director) {
   let searchDirector;
-
   searchDirector = array.filter((element)=>element.director == director);
-  
-  console.log(searchDirector);
-
   return searchDirector;
 }
 
@@ -31,39 +26,59 @@ function moviesAverageOfDirector(array, director) {
     return element.score;
   })
 
+  return moviesAverage(getScores);
+}
+
+function moviesAverage(getScores){
   totalScore =getScores.reduce(function(score,number){
     return (score+number)/getScores.length;
   });
-
-  console.log(totalScore.toFixed(2));
-  return totalScore.toFixed(2);  
+  
+  return Number(totalScore.toFixed(2));  
 }
 
 // Exercise 4:  Alphabetic order by title 
 function orderAlphabetically(array) {
   function titlesAlphabetically(x, y){
-    if (x.title < y.title) {return -1;}
-    if (x.title > y.title) {return 1;}
+    if (x < y) {return -1;}
+    if (x > y) {return 1;}
     return 0;
-}
-let orderTitles = array.sort(titlesAlphabetically).slice(0,20);
-console.log(orderTitles);
-return orderTitles;
+  }
+  let finalTitles=array.map((element) => {
+    return element.title;
+  })
+  let orderTitles = finalTitles.sort(titlesAlphabetically).slice(0,20);
+  //console.log(orderTitles);
+  return orderTitles;
 }
 
 // Exercise 5: Order by year, ascending
-function orderByYear() {
-
+function orderByYear(array) {
+  function orderYears(x, y){
+    if (x.year < y.year) {return -1;}
+    if (x.year > y.year) {return 1;}
+    if (x.title < y.title) {return -1;}
+    if (x.title > y.title) {return 1;}
+    return 0;
+  }
+  return array.sort(orderYears).slice();
 }
 
 // Exercise 6: Calculate the average of the movies in a category
-function moviesAverageByCategory() {
+function moviesAverageByCategory(array,genre) {
+  let findCategory = array.filter((element) => element.genre == genre);
 
+  let getScores = findCategory.filter(element => element.score != "").map((element) => {
+      return element.score;
+  })
+  
+  return moviesAverage(getScores);
 }
 
 // Exercise 7: Modify the duration of movies to minutes
 function hoursToMinutes() {
-
+  
+  
 }
 
 // Exercise 8: Get the best film of a year
